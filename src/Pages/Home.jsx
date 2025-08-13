849f019720b3295c72f94fb08a3920c1dda92f44
Dawit-coder <dawit3945@gmail.com>
2025-08-13 11:15:38 +0300

    Setup TailwindCSS and add StarBackground component

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import StarBackground from '../Components/StarBackground'
+
+const Home = () => {
+  return (
+    <div>
+      <StarBackground/>
+    </div>
+  )
+}
+
+export default Home
